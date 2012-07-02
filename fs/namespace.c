d79319d1a269957385f2f275c2b75e1afc1209d5
Jan Blunck <jblunck@suse.de>
2012-07-02 13:02:44 +0100

    union-mount: Introduce MNT_UNION and MS_UNION flags
    Add per mountpoint flag for Union Mount support.  You need additional patches
    to util-linux for that to work - see:
    
    git://git.kernel.org/pub/scm/utils/util-linux-ng/val/util-linux-ng.git
    
    Signed-off-by: Jan Blunck <jblunck@suse.de>
    Signed-off-by: Valerie Aurora <vaurora@redhat.com>
    Signed-off-by: David Howells <dhowells@redhat.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -2198,10 +2198,12 @@ long do_mount(char *dev_name, char *dir_name, char *type_page,
 		mnt_flags &= ~(MNT_RELATIME | MNT_NOATIME);
 	if (flags & MS_RDONLY)
 		mnt_flags |= MNT_READONLY;
+	if (flags & MS_UNION)
+		mnt_flags |= MNT_UNION;
 
 	flags &= ~(MS_NOSUID | MS_NOEXEC | MS_NODEV | MS_ACTIVE | MS_BORN |
 		   MS_NOATIME | MS_NODIRATIME | MS_RELATIME| MS_KERNMOUNT |
-		   MS_STRICTATIME);
+		   MS_STRICTATIME | MS_UNION);
 
 	if (flags & MS_REMOUNT)
 		retval = do_remount(&path, flags & ~MS_REMOUNT, mnt_flags,
