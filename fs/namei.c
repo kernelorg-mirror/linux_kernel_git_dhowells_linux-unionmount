03b132eab377d26666df70827f882616172061db
Valerie Aurora <vaurora@redhat.com>
2012-10-31 17:57:22 +0000

    union-mount: Create whiteout on rmdir()
    Whiteout a deleted directory in a union mounted file system.
    
    Original-author: Valerie Aurora <vaurora@redhat.com>
    Signed-off-by: David Howells <dhowells@redhat.com>

--- fs/namei.c
+++ fs/namei.c
@@ -4068,11 +4068,6 @@ static long do_rmdir(int dfd, const char __user *pathname)
 	if (IS_ERR(name))
 		return PTR_ERR(name);
 
-	/* rmdir() on union mounts not implemented yet */
-	error = -EINVAL;
-	if (IS_DIR_UNIONED(nd.path.dentry))
-		goto exit1;
-
 	switch(nd.last_type) {
 	case LAST_DOTDOT:
 		error = -ENOTEMPTY;
@@ -4101,6 +4096,10 @@ static long do_rmdir(int dfd, const char __user *pathname)
 	error = security_path_rmdir(&nd.path, path.dentry);
 	if (error)
 		goto exit3;
+	if (IS_DIR_UNIONED(nd.path.dentry)) {
+		error = do_whiteout(&nd, &path, 1);
+		goto exit3;
+	}
 	error = vfs_rmdir(nd.path.dentry->d_inode, path.dentry);
 exit3:
 	path_put_conditional(&path, &nd);
