1a22f201f998881266f78be2545354f58f7c9c1d
Jan Blunck <jblunck@suse.de>
2012-10-31 16:06:51 +0000

    whiteout: Allow removal of a directory with whiteouts
    do_whiteout() allows removal of a directory when it has whiteouts but
    is logically empty.
    
    XXX - This patch abuses readdir() to check if the union directory is
    logically empty - that is, all the entries are whiteouts (or "." or
    "..").  Currently, we have no clean VFS interface to ask the lower
    file system if a directory is empty.
    
    Fixes:
     - Add ->is_directory_empty() op
     - Add is_directory_empty flag to dentry (ugly dcache populate)
     - Ask underlying fs to remove it and look for an error return
     - (your idea here)
    
    Signed-off-by: Jan Blunck <jblunck@suse.de>
    Signed-off-by: Valerie Aurora <vaurora@redhat.com>
    Signed-off-by: David Howells <dhowells@redhat.com>

--- fs/namei.c
+++ fs/namei.c
@@ -34,6 +34,7 @@
 #include <linux/device_cgroup.h>
 #include <linux/fs_struct.h>
 #include <linux/posix_acl.h>
+#include <linux/init_task.h>
 #include <asm/uaccess.h>
 
 #include "internal.h"
@@ -3353,6 +3354,87 @@ error_unlock:
 		mutex_unlock(&old_inode->i_mutex);
 	if (do_dput)
 		dput(old_dentry);
+	return err;
+}
+
+/*
+ * XXX - We are abusing readdir to check if a union directory is
+ * logically empty.
+ */
+static int filldir_is_empty(void *__buf, const char *name, int namelen,
+			    loff_t offset, u64 ino, unsigned int d_type)
+{
+	int *is_empty = __buf;
+
+	switch (namelen) {
+	case 2:
+		if (name[1] != '.')
+			break;
+	case 1:
+		if (name[0] != '.')
+			break;
+		return 0;
+	}
+
+	if (d_type == DT_WHT)
+		return 0;
+
+	*is_empty = 0;
+	return 1; /* no point scanning further */
+}
+
+static int directory_is_empty(struct path *path)
+{
+	struct file *file;
+	int err;
+	int is_empty = 1;
+
+	BUG_ON(!S_ISDIR(path->dentry->d_inode->i_mode));
+
+	file = dentry_open(path, O_RDONLY, &init_cred);
+	if (IS_ERR(file))
+		return 0;
+
+	err = vfs_readdir(file, filldir_is_empty, &is_empty);
+
+	fput(file);
+	return is_empty;
+}
+
+static int do_whiteout(struct nameidata *nd, struct path *path, int isdir)
+{
+	struct path safe = nd->path;
+	struct dentry *dentry = path->dentry;
+	int err;
+
+	path_get(&safe);
+
+	err = may_delete(nd->path.dentry->d_inode, dentry, isdir);
+	if (err)
+		goto out;
+
+	err = -ENOTEMPTY;
+	if (isdir && !directory_is_empty(path))
+		goto out;
+
+	if (nd->path.dentry != dentry->d_parent) {
+		dentry = __lookup_hash(&path->dentry->d_name, nd->path.dentry,
+				       nd->flags);
+		err = PTR_ERR(dentry);
+		if (IS_ERR(dentry))
+			goto out;
+
+		dput(path->dentry);
+		if (path->mnt != safe.mnt)
+			mntput(path->mnt);
+		path->mnt = nd->path.mnt;
+		path->dentry = dentry;
+	}
+
+	err = vfs_whiteout(nd->path.dentry, dentry, isdir);
+
+out:
+	path_put(&safe);
 	return err;
 }
 
